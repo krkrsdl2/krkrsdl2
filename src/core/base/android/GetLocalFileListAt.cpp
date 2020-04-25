19fa23bd1824f5c68a0a01b059b51c2e3a6c7d73
uyjulian <uyjulian@gmail.com>
2020-04-25 18:10:23 -0500

    Fix compilation of GetLocalFileListAt for Linux

--- src/core/base/android/GetLocalFileListAt.cpp
+++ src/core/base/android/GetLocalFileListAt.cpp
@@ -6,9 +6,11 @@
 #include "GetLocalFileListAt.h"
 #include "CharacterSet.h"
 
+#ifdef st_atime
 #undef st_atime
 #undef st_ctime
 #undef st_mtime
+#endif
 
 struct tTVP_stat {
 	uint16_t st_mode;
@@ -25,9 +27,19 @@ static bool TVP_stat(const char *name, tTVP_stat &s) {
 	bool ret = !stat(name, &t);
 	s.st_mode = t.st_mode;
 	s.st_size = t.st_size;
+#ifdef __MACH__
 	s.st_atime = t.st_atimespec.tv_sec;
 	s.st_mtime = t.st_mtimespec.tv_sec;
 	s.st_ctime = t.st_ctimespec.tv_sec;
+#elif defined(__linux__)
+	s.st_atime = t.st_atim.tv_sec;
+	s.st_mtime = t.st_mtim.tv_sec;
+	s.st_ctime = t.st_ctim.tv_sec;
+#else
+	s.st_atime = t.st_atime;
+	s.st_mtime = t.st_mtime;
+	s.st_ctime = t.st_ctime;
+#endif
 	return ret;
 }
 
