9dd6235b2f7f7ee5b327d33223e242432889df9c
uyjulian <uyjulian@gmail.com>
2020-06-10 16:09:19 -0500

    Get rid of dirlist and related code

--- src/core/base/android/StorageImpl.h
+++ src/core/base/android/StorageImpl.h
@@ -17,22 +17,6 @@
 #include <stdio.h>
 
 
-#ifndef S_IFMT
-#define S_IFDIR  0x4000 // Directory
-#define S_IFREG  0x8000 // Regular
-#endif
-
-struct tTVPLocalFileInfo {
-	const char * NativeName;
-	unsigned short Mode; // S_IFMT
-	tjs_uint64         Size;
-	time_t         AccessTime;
-	time_t         ModifyTime;
-	time_t         CreationTime;
-};
-
-void TVPGetLocalFileListAt(const ttstr &name, const std::function<void(const ttstr&, tTVPLocalFileInfo*)>& cb);
-
 
 //---------------------------------------------------------------------------
 // tTVPLocalFileStream
