daadcea04bdfbf9f5b8bb87c70c754d4857da174
uyjulian <uyjulian@gmail.com>
2023-02-19 10:06:58 -0600

    makestub: reduce global variable usage

--- src/core/base/sdl2/makestub.py
+++ src/core/base/sdl2/makestub.py
@@ -84,7 +84,7 @@ def get_ret_type(type_, prefix):
 
 
 
-def make_func_stub(rettype, name, arg, type_, prefix, isconst, isstatic):
+def make_func_stub(func_list, h_stub, rettype, name, arg, type_, prefix, isconst, isstatic):
 	rettype = re.sub(r"\n", r" ", rettype, flags=re.S) # g
 	rettype = re.sub(r"\t", r" ", rettype, flags=re.S) # g
 	name = re.sub(r"\n", r" ", name, flags=re.S) # g
@@ -188,18 +188,18 @@ def make_func_stub(rettype, name, arg, type_, prefix, isconst, isstatic):
 	h_stub.append(h)
 	# num += 1
 
-def list_func_stub(prefix, content, type_):
+def list_func_stub(func_list, h_stub, prefix, content, type_):
 	for match_obj in re.finditer(prefix + r"_METHOD_DEF\(\s*(.*?)\s*,\s*(.*?)\s*,\s*\((.*?)\)\s*\)", content, flags=re.S): # g
-		make_func_stub(match_obj.group(1), match_obj.group(2), match_obj.group(3), type_, prefix, False, False)
+		make_func_stub(func_list=func_list, h_stub=h_stub, rettype=match_obj.group(1), name=match_obj.group(2), arg=match_obj.group(3), type_=type_, prefix=prefix, isconst=False, isstatic=False)
 	for match_obj in re.finditer(prefix + r"_CONST_METHOD_DEF\(\s*(.*?)\s*,\s*(.*?)\s*,\s*\((.*?)\)\s*\)", content, flags=re.S): # g
-		make_func_stub(match_obj.group(1), match_obj.group(2), match_obj.group(3), type_, prefix, True, False)
+		make_func_stub(func_list=func_list, h_stub=h_stub, rettype=match_obj.group(1), name=match_obj.group(2), arg=match_obj.group(3), type_=type_, prefix=prefix, isconst=True,  isstatic=False)
 	for match_obj in re.finditer(prefix + r"_STATIC_METHOD_DEF\(\s*(.*?)\s*,\s*(.*?)\s*,\s*\((.*?)\)\s*\)", content, flags=re.S): # g
-		make_func_stub(match_obj.group(1), match_obj.group(2), match_obj.group(3), type_, prefix, False, True)
+		make_func_stub(func_list=func_list, h_stub=h_stub, rettype=match_obj.group(1), name=match_obj.group(2), arg=match_obj.group(3), type_=type_, prefix=prefix, isconst=False, isstatic=True)
 	for match_obj in re.finditer(prefix + r"_STATIC_CONST_METHOD_DEF\(\s*(.*?)\s*,\s*(.*?)\s*,\s*\((.*?)\)\s*\)", content, flags=re.S): # g
-		make_func_stub(match_obj.group(1), match_obj.group(2), match_obj.group(3), type_, prefix, True, True)
+		make_func_stub(func_list=func_list, h_stub=h_stub, rettype=match_obj.group(1), name=match_obj.group(2), arg=match_obj.group(3), type_=type_, prefix=prefix, isconst=True,  isstatic=True)
 
 
-def make_exp_stub(rettype, name, arg):
+def make_exp_stub(func_list, rettype, name, arg):
 	rettype = re.sub(r"\n", r" ", rettype, flags=re.S) # g
 	rettype = re.sub(r"\t", r" ", rettype, flags=re.S) # g
 	name = re.sub(r"\n", r" ", name, flags=re.S) # g
@@ -242,7 +242,7 @@ def make_exp_stub(rettype, name, arg):
 	func_list.append(mangled)
 	# num += 1
 
-def process_exp_stub(file):
+def process_exp_stub(defs, impls, func_list, file):
 
 	content = ""
 	with open(file, "r") as f:
@@ -253,9 +253,9 @@ def process_exp_stub(file):
 	for match_obj in re.finditer(r"\/\*\[C\*\/(.*?)\/\*C]\*\/", content, flags=re.S): # g
 		impls.append(match_obj.group(1))
 	for match_obj in re.finditer(r"TJS_EXP_FUNC_DEF\(\s*(.*?)\s*,\s*(.*?)\s*,\s*\((.*?)\)\s*\)", content, flags=re.S): # g
-		make_exp_stub(match_obj.group(1), match_obj.group(2), match_obj.group(3))
+		make_exp_stub(func_list=func_list, rettype=match_obj.group(1), name=match_obj.group(2), arg=match_obj.group(3))
 	for match_obj in re.finditer(r"TVP_GL_FUNC_PTR_EXTERN_DECL\(\s*(.*?)\s*,\s*(.*?)\s*,\s*\((.*?)\)\s*\)", content, flags=re.S): # g
-		make_exp_stub(match_obj.group(1), match_obj.group(2), match_obj.group(3))
+		make_exp_stub(func_list=func_list, rettype=match_obj.group(1), name=match_obj.group(2), arg=match_obj.group(3))
 
 # undef $/
 func_list = []
@@ -288,206 +288,202 @@ ofh.write("""
 
 fh = open("../../tjs2/tjsVariant.h")
 content = fh.read()
+
+method_list = []
+
 srch = re.search(r"\/\*start-of-tTJSVariant\*\/(.*?)\/\*end-of-tTJSVariant\*\/", content, flags=re.S)
-h_stub = []
-list_func_stub("TJS", srch.group(1), "tTJSVariant")
-variant = h_stub
+variant = []
+list_func_stub(func_list=method_list, h_stub=variant, prefix="TJS", content=srch.group(1), type_="tTJSVariant")
 
 srch = re.search(r"\/\*start-of-tTJSVariantOctet\*\/(.*?)\/\*end-of-tTJSVariantOctet\*\/", content, flags=re.S)
-h_stub = []
-list_func_stub("TJS", srch.group(1), "tTJSVariantOctet")
-variantoctet = h_stub
+variantoctet = []
+list_func_stub(func_list=method_list, h_stub=variantoctet, prefix="TJS", content=srch.group(1), type_="tTJSVariantOctet")
+
 
 fh = open("../../tjs2/tjsString.h")
 content = fh.read()
 srch = re.search(r"\/\*start-of-tTJSString\*\/(.*?)\/\*end-of-tTJSString\*\/", content, flags=re.S)
-h_stub = []
-list_func_stub("TJS", srch.group(1), "tTJSString")
-string_ = h_stub
+string_ = []
+list_func_stub(func_list=method_list, h_stub=string_, prefix="TJS", content=srch.group(1), type_="tTJSString")
+
 
 fh = open("../../tjs2/tjsVariantString.h")
 content = fh.read()
 srch = re.search(r"\/\*start-of-tTJSVariantString\*\/(.*?)\/\*end-of-tTJSVariantString\*\/", content, flags=re.S)
-h_stub = []
-list_func_stub("TJS", srch.group(1), "tTJSVariantString")
-variantstring = h_stub
+variantstring = []
+list_func_stub(func_list=method_list, h_stub=variantstring, prefix="TJS", content=srch.group(1), type_="tTJSVariantString")
 
-defs = []
+
+defs_system = []
 impls = []
-
-method_list = func_list
 
 func_list = []
 
 ofh.write("#include \"tjsTypes.h\"\n")
-process_exp_stub("../../tjs2/tjsTypes.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsTypes.h")
 
 ofh.write("#include \"tjsConfig.h\"\n")
-process_exp_stub("../../tjs2/tjsConfig.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsConfig.h")
 
 ofh.write("#include \"tjsVariantString.h\"\n")
-process_exp_stub("../../tjs2/tjsVariantString.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsVariantString.h")
 
 ofh.write("#include \"tjsUtils.h\"\n")
-process_exp_stub("../../tjs2/tjsUtils.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsUtils.h")
 
 ofh.write("#include \"tjsString.h\"\n")
-process_exp_stub("../../tjs2/tjsString.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsString.h")
 
 ofh.write("#include \"tjsInterface.h\"\n")
-process_exp_stub("../../tjs2/tjsInterface.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsInterface.h")
 
 ofh.write("#include \"tjsErrorDefs.h\"\n")
-process_exp_stub("../../tjs2/tjsErrorDefs.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsErrorDefs.h")
 
 ofh.write("#include \"tjsNative.h\"\n")
-process_exp_stub("../../tjs2/tjsNative.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsNative.h")
 
 ofh.write("#include \"tjsVariant.h\"\n")
-process_exp_stub("../../tjs2/tjsVariant.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsVariant.h")
 
 ofh.write("#include \"tjsArray.h\"\n")
-process_exp_stub("../../tjs2/tjsArray.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsArray.h")
 
 ofh.write("#include \"tjsDictionary.h\"\n")
-process_exp_stub("../../tjs2/tjsDictionary.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsDictionary.h")
 
 ofh.write("#include \"tjs.h\"\n")
-process_exp_stub("../../tjs2/tjs.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjs.h")
 
 ofh.write("#include \"tjsMessage.h\"\n")
-process_exp_stub("../../tjs2/tjsMessage.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsMessage.h")
 
 ofh.write("#include \"tjsGlobalStringMap.h\"\n")
-process_exp_stub("../../tjs2/tjsGlobalStringMap.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsGlobalStringMap.h")
 
 ofh.write("#include \"tjsObject.h\"\n")
-process_exp_stub("../../tjs2/tjsObject.h")
-process_exp_stub("../../tjs2/tjsObject.cpp")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsObject.h")
+process_exp_stub(defs=defs_system, impls=impls, func_list=func_list, file="../../tjs2/tjsObject.cpp")
 
-defs_system = "\n".join(defs) + "\n"
-defs = []
+defs_misc = []
 
 
 ofh.write("#include \"StorageIntf.h\"\n")
-process_exp_stub("../StorageIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../StorageIntf.h")
 
 ofh.write("#include \"TextStream.h\"\n")
-process_exp_stub("../TextStream.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../TextStream.h")
 
 ofh.write("#include \"CharacterSet.h\"\n")
-process_exp_stub("../CharacterSet.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../CharacterSet.h")
 
 ofh.write("#include \"XP3Archive.h\"\n")
-process_exp_stub("../XP3Archive.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../XP3Archive.h")
 
 ofh.write("#include \"EventIntf.h\"\n")
-process_exp_stub("../EventIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../EventIntf.h")
 
 ofh.write("#include \"SystemIntf.h\"\n")
-process_exp_stub("../SystemIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../SystemIntf.h")
 
 ofh.write("#include \"SystemImpl.h\"\n")
-process_exp_stub("./SystemImpl.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="./SystemImpl.h")
 
 ofh.write("#include \"ScriptMgnIntf.h\"\n")
-process_exp_stub("../ScriptMgnIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../ScriptMgnIntf.h")
 
 ofh.write("#include \"BinaryStream.h\"\n")
-process_exp_stub("../BinaryStream.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../BinaryStream.h")
 
 ofh.write("#include \"StorageImpl.h\"\n")
-process_exp_stub("../android/StorageImpl.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../android/StorageImpl.h")
 
 ofh.write("#include \"PluginImpl.h\"\n")
-process_exp_stub("../android/PluginImpl.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../android/PluginImpl.h")
 
 ofh.write("#include \"SysInitIntf.h\"\n")
-process_exp_stub("../SysInitIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../SysInitIntf.h")
 
 ofh.write("#include \"SysInitImpl.h\"\n")
-process_exp_stub("../android/SysInitImpl.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../android/SysInitImpl.h")
 
 ofh.write("#include \"DetectCPU.h\"\n")
-process_exp_stub("../../environ/android/DetectCPU.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../environ/android/DetectCPU.h")
 
 ofh.write("#include \"ThreadIntf.h\"\n")
-process_exp_stub("../../utils/ThreadIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../utils/ThreadIntf.h")
 
 ofh.write("#include \"DebugIntf.h\"\n")
-process_exp_stub("../../utils/DebugIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../utils/DebugIntf.h")
 
 ofh.write("#include \"Random.h\"\n")
-process_exp_stub("../../utils/Random.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../utils/Random.h")
 
 ofh.write("#include \"ClipboardIntf.h\"\n")
-process_exp_stub("../../utils/ClipboardIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../utils/ClipboardIntf.h")
 
 ofh.write("#include \"TickCount.h\"\n")
-process_exp_stub("../../utils/TickCount.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../utils/TickCount.h")
 
 ofh.write("#include \"MsgIntf.h\"\n")
-process_exp_stub("../../msg/MsgIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../msg/MsgIntf.h")
 
 ofh.write("#include \"WaveIntf.h\"\n")
-process_exp_stub("../../sound/WaveIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../sound/WaveIntf.h")
 
 ofh.write("#include \"GraphicsLoaderIntf.h\"\n")
-process_exp_stub("../../visual/GraphicsLoaderIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/GraphicsLoaderIntf.h")
 
 ofh.write("#include \"tvpfontstruc.h\"\n")
-process_exp_stub("../../visual/tvpfontstruc.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/tvpfontstruc.h")
 
 ofh.write("#include \"tvpinputdefs.h\"\n")
-process_exp_stub("../../visual/tvpinputdefs.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/tvpinputdefs.h")
 
 ofh.write("#include \"LayerBitmapIntf.h\"\n")
-process_exp_stub("../../visual/LayerBitmapIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/LayerBitmapIntf.h")
 
 ofh.write("#include \"drawable.h\"\n")
-process_exp_stub("../../visual/drawable.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/drawable.h")
 
 ofh.write("#include \"ComplexRect.h\"\n")
-process_exp_stub("../../visual/ComplexRect.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/ComplexRect.h")
 
 ofh.write("#include \"LayerIntf.h\"\n")
-process_exp_stub("../../visual/LayerIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/LayerIntf.h")
 
 ofh.write("#include \"LayerManager.h\"\n")
-process_exp_stub("../../visual/LayerManager.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/LayerManager.h")
 
 ofh.write("#include \"WindowIntf.h\"\n")
-process_exp_stub("../../visual/WindowIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/WindowIntf.h")
 
 ofh.write("#include \"WindowImpl.h\"\n")
-process_exp_stub("../../visual/android/WindowImpl.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/android/WindowImpl.h")
 
 # ofh.write("#include \"DrawDevice.h\"\n")
-# process_exp_stub("../../visual/DrawDevice.h")
+# process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/DrawDevice.h")
 
 ofh.write("#include \"voMode.h\"\n")
-process_exp_stub("../../visual/voMode.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/voMode.h")
 
 ofh.write("#include \"VideoOvlIntf.h\"\n")
-process_exp_stub("../../visual/VideoOvlIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/VideoOvlIntf.h")
 
 ofh.write("#include \"TransIntf.h\"\n")
-process_exp_stub("../../visual/TransIntf.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/TransIntf.h")
 
 ofh.write("#include \"transhandler.h\"\n")
-process_exp_stub("../../visual/transhandler.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/transhandler.h")
 
 ofh.write("#include \"tvpgl.h\"\n")
-process_exp_stub("../../visual/tvpgl.h")
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/tvpgl.h")
 
 # ofh.write("#include \"tvpgl_ia32_intf.h\"\n")
-# process_exp_stub("../../visual/IA32/tvpgl_ia32_intf.h")
+# process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/IA32/tvpgl_ia32_intf.h")
 
 ofh.write("#include \"OpenGLHeader.h\"\n")
-process_exp_stub("../../visual/opengl/OpenGLHeader.h")
-
-defs_misc = "\n".join(defs) + "\n"
-defs = []
+process_exp_stub(defs=defs_misc, impls=impls, func_list=func_list, file="../../visual/opengl/OpenGLHeader.h")
 
 all_list = [*method_list, *func_list]
 
@@ -613,7 +609,7 @@ ohfh.write("""\
 
 """)
 
-ohfh.write(defs_system)
+ohfh.write("\n".join(defs_system) + "\n")
 
 ohfh.write("""
 #ifdef __BORLANDC__
@@ -775,7 +771,7 @@ ohfh.write("""\
 
 """)
 
-ohfh.write(defs_misc)
+ohfh.write("\n".join(defs_misc) + "\n")
 
 ohfh.write("""\
 //---------------------------------------------------------------------------
