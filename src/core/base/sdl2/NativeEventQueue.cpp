047413f377fc5204c2d2478240b09624712a2541
Julian Uy <uyjulian@gmail.com>
2026-04-30 22:53:45 -0500

    cleanup: return early

--- src/core/base/sdl2/NativeEventQueue.cpp
+++ src/core/base/sdl2/NativeEventQueue.cpp
@@ -77,36 +77,38 @@ void NativeEventQueueImplement::PostEvent( const NativeEvent& event ) {
 tjs_uint32 NativeEventQueueImplement::native_event_queue_custom_event_type = 0;
 
 NativeEventQueueImplement::NativeEventQueueImplement() {
-	if (NativeEventQueueImplement::native_event_queue_custom_event_type == 0)
+	if (NativeEventQueueImplement::native_event_queue_custom_event_type != 0)
 	{
-		if (SDL_WasInit(SDL_INIT_EVENTS) == 0)
-		{
-			SDL_Init(SDL_INIT_EVENTS);
-		}
-		NativeEventQueueImplement::native_event_queue_custom_event_type = SDL_RegisterEvents(1);
+		return;
 	}
+	if (SDL_WasInit(SDL_INIT_EVENTS) == 0)
+	{
+		SDL_Init(SDL_INIT_EVENTS);
+	}
+	NativeEventQueueImplement::native_event_queue_custom_event_type = SDL_RegisterEvents(1);
 }
 
 void NativeEventQueueImplement::PostEvent(const NativeEvent& ev) {
-	if (NativeEventQueueImplement::native_event_queue_custom_event_type != 0)
+	if (NativeEventQueueImplement::native_event_queue_custom_event_type == 0)
 	{
-		NativeEvent * tmp_ev = new NativeEvent(ev);
-		SDL_Event event;
-		SDL_memset(&event, 0, sizeof(event));
-		event.type = NativeEventQueueImplement::native_event_queue_custom_event_type;
-		event.user.code = 0;
-		event.user.data2 = (void *)tmp_ev;
-		tmp_ev->SetQueue(this);
-		int res = 0;
-		while (true)
+		return;
+	}
+	NativeEvent * tmp_ev = new NativeEvent(ev);
+	SDL_Event event;
+	SDL_memset(&event, 0, sizeof(event));
+	event.type = NativeEventQueueImplement::native_event_queue_custom_event_type;
+	event.user.code = 0;
+	event.user.data2 = (void *)tmp_ev;
+	tmp_ev->SetQueue(this);
+	int res = 0;
+	while (true)
+	{
+		res = SDL_PushEvent(&event);
+		if (res == 1)
 		{
-			res = SDL_PushEvent(&event);
-			if (res == 1)
-			{
-				break;
-			}
-			SDL_Delay(100);
+			break;
 		}
+		SDL_Delay(100);
 	}
 }
 #endif
